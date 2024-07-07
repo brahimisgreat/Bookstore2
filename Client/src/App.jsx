e367fa11818e30dd47dc1c464585b2310e33acd9
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-07-06 23:55:48 -0400

    added rputes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Books } from "./Pages/Books";
+import { Add } from "./Pages/Add";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>} />
+          <Route path="/" element={<Add/>} />
+          <Route path="/" element={<Books/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Client/src/Pages/Books.jsx
@@ -0,0 +1,8 @@
+
+export const Books = () => {
+  return (
+    <div className="books">
+    Books
+    </div>
+  )
+}
